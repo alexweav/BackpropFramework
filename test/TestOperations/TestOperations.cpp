2cfd4cbbf399f3e5551fe195e7a1cf954bf331f4
alexweav <alexander.weaver@ttu.edu>
2017-08-07 20:57:56 -0500

    Fixed linking issue between test assemblies

--- test/TestOperations/TestOperations.cpp
+++ test/TestOperations/TestOperations.cpp
@@ -1,5 +1,16 @@
 #include "gtest/gtest.h"
+#include "Operations/Constant.h"
+#include "Evaluation/ForwardEvaluator.h"
+#include "Utils/Dictionary.h"
 
 TEST(TestTests, Test1) {
     EXPECT_EQ(1, 1);
 }
+
+TEST(ConstantTests, ForwardTest) {
+    auto cons = new Constant(2.0);
+    auto eval = new ForwardEvaluator();
+    Dictionary<Input*, float> inputs;
+    EXPECT_EQ(eval->ForwardEvaluate(cons, inputs), 2.0);
+}
+
