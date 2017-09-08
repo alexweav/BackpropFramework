647fe2792560f3abc4294bceedda8d23da8bf22f
alexweav <alexander.weaver@ttu.edu>
2017-09-08 15:07:09 -0500

    Created IInitializer and a Zeros initializer

---
+++ src/Data/Initializers/Zeros.cpp
@@ -0,0 +1,5 @@
+#include "Data/Initializers/Zeros.h"
+
+namespace Initializers {
+
+}
---
+++ src/Data/Initializers/Zeros.h
@@ -0,0 +1,13 @@
+#ifndef SRC_DATA_INITIALIZERS_ZEROS_H_
+#define SRC_DATA_INITIALIZERS_ZEROS_H_
+
+#include "Data/IInitializer.h"
+
+namespace Initializers {
+    
+    class Zeros : public IInitializer {
+
+    };
+}
+
+#endif
