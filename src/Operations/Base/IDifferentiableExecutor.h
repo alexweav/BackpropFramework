cb3ebcb0d021ecc9a5c738a250ea67959f020f31
alexweav <alexander.weaver@ttu.edu>
2018-01-17 01:14:26 -0600

    semicolon

--- src/Operations/Base/IDifferentiableExecutor.h
+++ src/Operations/Base/IDifferentiableExecutor.h
@@ -7,6 +7,6 @@ class IDifferentiableExecutor: public virtual IExecutor {
  public:
     virtual DataObject operator() (const std::vector<DataObject>& inputs) const = 0;
     virtual std::vector<DataObject> Differentiate(const std::vector<DataObject>& prevInputs, const DataObject& dOut) const = 0;
-}
+};
 
 #endif  // SRC_OPERATIONS_BASE_IDIFFERENTIABLEEXECUTOR_H_
