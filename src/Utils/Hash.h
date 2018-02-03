f44381c56d0d915c29c12ba4ba320a3ad569740d
alexweav <alexander.weaver@ttu.edu>
2018-02-03 01:10:32 -0600

    Styling in Utils directory

--- src/Utils/Hash.h
+++ src/Utils/Hash.h
@@ -3,20 +3,21 @@
 
 namespace utils {
 
-    inline size_t HashCombine(const size_t& val1, const size_t& val2) {
-        return val1 ^ (val2 + 0x9e3779b9 + (val1<<6) + (val1>>2));
-    }
-
-    template<typename T>
-    struct PointerHash {
-        inline size_t operator() (const T* ptr) const {
-            auto addr = reinterpret_cast<uintptr_t>(ptr);
-#if SIZE_MAX < UINTPTR_MAX
-            addr %= SIZE_MAX;
-#endif
-            return addr;
-        }
-    };
+inline size_t HashCombine(const size_t& val1, const size_t& val2) {
+    return val1 ^ (val2 + 0x9e3779b9 + (val1<<6) + (val1>>2));
 }
+
+template<typename T>
+struct PointerHash {
+    inline size_t operator() (const T* ptr) const {
+        auto addr = reinterpret_cast<uintptr_t>(ptr);
+#if SIZE_MAX < UINTPTR_MAX
+        addr %= SIZE_MAX;
+#endif
+        return addr;
+    }
+};
+
+}   // namespace utils
 
 #endif  // SRC_UTILS_HASH_H_
