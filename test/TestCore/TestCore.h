03c125b78a7e5757c3ad1ce293db8211eca5fa96
alexweav <alexander.weaver@ttu.edu>
2018-03-18 17:28:04 -0500

    Add tests for differentiability tracking

--- test/TestCore/TestCore.h
+++ test/TestCore/TestCore.h
@@ -3,16 +3,26 @@
 
 #include "gtest/gtest.h"
 
+#include "Operations/Arithmetic.h"
 #include "Operations/Value.h"
+
+#include "FakeNonDifferentiableConstant.h"
 
 class DifferentiabilityTest: public ::testing::Test {
  protected:
     virtual void SetUp() {
         cons1 = Value(1.0);
         cons2 = Value(2.0);
+        nonDiffCons1 = std::shared_ptr<FakeNonDifferentiableConstant>(new FakeNonDifferentiableConstant(1.0));
+        addDiff = Add(cons1, cons2);
+        addNonDiffComposition = Add(nonDiffCons1, cons2);
     }
     NodePtr cons1;
     NodePtr cons2;
+    std::shared_ptr<FakeNonDifferentiableConstant> nonDiffCons1;
+    NodePtr addDiff;
+    NodePtr addNonDiffComposition;
+
 };
 
 #endif  // TEST_TESTCORE_TESTCORE_H_
